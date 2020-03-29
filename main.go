96b90d0ea28c8f40a0654e86dd9e9fc7b2ccae1a
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-03-29 18:48:18 +0200

    AWS authentication works

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/bigkevmcd/go-configparser"
 	"github.com/manifoldco/promptui"
 )
 
-const credentialFile string = "/.aws/credentials"
+const (
+	credentialFile string = "/.aws/credentials"
+)
+
+func removeBadProfile(list *configparser.ConfigParser) []string {
+	return list.Sections()
+}
 
 func main() {
-	var home string
+	var (
+		home        string
+		profileList []string
+	)
+
 	fmt.Println("Auth cli with mfa project")
 	home, err := os.UserHomeDir()
 	if err != nil {
@@ -24,10 +39,11 @@ func main() {
 		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
+	profileList = removeBadProfile(config)
 
 	prompt := promptui.Select{
 		Label: "Select profile",
-		Items: config.Sections(),
+		Items: profileList,
 	}
 
 	_, result, err := prompt.Run()
@@ -39,4 +55,43 @@ func main() {
 
 	fmt.Printf("You choose %q\n", result)
 
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Enter your aws username: ")
+	scanner.Scan()
+	username := scanner.Text()
+
+	fmt.Println("Enter your token: ")
+	scanner.Scan()
+	token := scanner.Text()
+
+	awsSession, err := session.NewSessionWithOptions(session.Options{
+		Profile: result,
+	})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+	svcSts := sts.New(awsSession)
+
+	identity, err := svcSts.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+	fmt.Println(identity.Account)
+
+	tmpSession, err := svcSts.GetSessionToken(&sts.GetSessionTokenInput{
+		DurationSeconds: aws.Int64(3600),
+		SerialNumber:    aws.String("arn:aws:iam::" + *identity.Account + ":mfa/" + username),
+		TokenCode:       aws.String(token),
+	})
+	if err != nil {
+		if awsErr, ok := err.(awserr.Error); ok {
+			fmt.Println(awsErr)
+		}
+	}
+	fmt.Println(tmpSession)
 }
