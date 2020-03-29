d81f60557211efd2b6627605ff0a16dd4a937f2e
Paloth <lth.paul@hotmail.fr>
2020-03-29 14:12:39 +0200

    Add promptui package

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module aws-mfa-cli
 
 go 1.14
 
-require github.com/bigkevmcd/go-configparser v0.0.0-20200217161103-d137835d2579
+require (
+	github.com/bigkevmcd/go-configparser v0.0.0-20200217161103-d137835d2579
+	github.com/manifoldco/promptui v0.7.0
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/bigkevmcd/go-configparser"
+	"github.com/manifoldco/promptui"
 )
 
 const credentialFile string = "/.aws/credentials"
@@ -14,14 +15,28 @@ func main() {
 	fmt.Println("Auth cli with mfa project")
 	home, err := os.UserHomeDir()
 	if err != nil {
-		fmt.Println("Error while get the home folder")
+		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
 
 	config, err := configparser.NewConfigParserFromFile(home + credentialFile)
 	if err != nil {
-		fmt.Println("Error while get the aws profiles")
+		fmt.Println("Error: ", err)
 		os.Exit(2)
 	}
-	fmt.Println(config.Sections())
+
+	prompt := promptui.Select{
+		Label: "Select profile",
+		Items: config.Sections(),
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
 }
