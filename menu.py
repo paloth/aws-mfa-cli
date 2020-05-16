27bacb0045264bba1ed1279a589f40c5f70d05b6
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-05-16 19:54:58 +0200

    [Rework] Separate menu function

---
+++ menu.py
@@ -0,0 +1,33 @@
+# import os
+import subprocess
+
+
+def filter_profile(config):
+    return [
+        profile
+        for profile in config.sections()
+        if not config.has_option(profile, "source_profile")
+        and not profile.endswith("-tmp")
+    ]
+
+
+def start(config):
+    flag = False
+    profiles = filter_profile(config)
+    while flag is False:
+        for index, profile in enumerate(profiles):
+            print(f"[{index + 1}] - {profile}\r")
+        try:
+            user_selection = input(
+                "Please select a profile to use to get your temporary credentials:\n"
+            )
+            while int(user_selection) == 0 or int(user_selection) > len(profiles):
+                raise ValueError
+        except ValueError:
+            subprocess.call("clear")
+            print("Bad selection!\nPlease select a profile listed below")
+            continue
+        flag = True
+    print(f"You selected the profile: {profiles[int(user_selection) - 1]}")
+    # os.environ["AWS_PROFILE"] = profiles[int(user_selection) - 1]
+    return profiles[int(user_selection) - 1]
