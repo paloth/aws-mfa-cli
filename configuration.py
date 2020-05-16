4386a85e5ceba0dab87f3d5f217125057cfdf4fe
Paul LOUIS THERESE <paul.louis-therese@d2si.io>
2020-05-16 19:55:23 +0200

    [Rework] Separate configuration function

---
+++ configuration.py
@@ -0,0 +1,25 @@
+def write(path, profile, config, credentials):
+    if profile + "-tmp" not in config:
+        config[f"{profile}-tmp"] = {}
+    config[f"{profile}-tmp"]["aws_access_key_id"] = credentials["Credentials"][
+        "AccessKeyId"
+    ]
+    config[f"{profile}-tmp"]["aws_secret_access_key"] = credentials["Credentials"][
+        "SecretAccessKey"
+    ]
+    config[f"{profile}-tmp"]["aws_session_token"] = credentials["Credentials"][
+        "SessionToken"
+    ]
+    config[f"{profile}-tmp"]["aws_default_region"] = "eu-west-1"
+    with open(path, "w") as configfile:
+        config.write(configfile)
+    return True
+
+
+def update_profile(path, profile, config, key):
+    config[profile]["aws_access_key_id"] = key["AccessKey"]["AccessKeyId"]
+    config[profile]["aws_secret_access_key"] = key["AccessKey"]["SecretAccessKey"]
+    config[profile]["aws_default_region"] = "eu-west-1"
+    with open(path, "w") as configfile:
+        config.write(configfile)
+    return True
